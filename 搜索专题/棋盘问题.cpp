7a30703c54f2e0a1708e7228d49c0742996a1868
GuosiHg <1142570567@qq.com>
2018-03-22 22:17:41 +0800

    添加算法学习目录

---
+++ 搜索专题/棋盘问题.cpp
@@ -0,0 +1,12 @@
+#include "iostream"
+using namespace std;
+
+int main()
+{
+    int n,k;
+    while(cin >> n >> k)
+    {
+        if(n == -1 && k == -1) break;
+        cout <<"11" <<endl;
+    }
+}
