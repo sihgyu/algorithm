f87c072e9e852ec1e8b5e7438d4638d57fa39e01
GuosiHg <1142570567@qq.com>
2018-04-10 16:50:43 +0800

    修改

--- 搜索专题/全排列dfs.cpp
+++ 搜索专题/全排列dfs.cpp
@@ -13,28 +13,40 @@ void print_ans()
     {
         printf("%d",answer[i]);
     }
+    printf("\n");
 }
 
 
-bool dfs(int now,int deep)
+void dfs(int now,int deep)
 {
-    if(used[now] == true )
-        return true;  // 如果点遍历过则返回
-    used[now] = true;
-    answer[deep] = now;
-    if(deep == n)
-    {
+    if(deep == n){
         print_ans();
-        used[now] = false;
-        return true;
+        return;
     }
-
-    for(int i = 1;i <= n;i++)
-    {
-        if(i == now )
-            continue ;
+    for(int i = 1;i <= n;i++){
+        if(used[i] == true) continue;
+        used[i] = true;
+        answer[deep+1] = i;
         dfs(i,deep+1);
+        used[i] = false;
     }
+    // if(used[now] == true )
+    //     return true;  // 如果点遍历过则返回
+    // used[now] = true;
+    // answer[deep] = now;
+    // if(deep == n)
+    // {
+    //     print_ans();
+    //     used[now] = false;
+    //     return true;
+    // }
+
+    // for(int i = 1;i <= n;i++)
+    // {
+    //     if(i == now )
+    //         continue ;
+    //     dfs(i,deep+1);
+    // }
 
     
 }
@@ -43,6 +55,10 @@ void judge()
 {
     for (int i = 1;i <= n;i++)
     {
+        memset(used,false,sizeof(used));
+        memset(answer,0,sizeof(answer));
+        used[i] = true;
+        answer[1] = i;
         dfs(i,1);
     }
 }
