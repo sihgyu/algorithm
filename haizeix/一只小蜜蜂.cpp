ed6fd9ab80cb9109c15269e534b5a07f21fee97d
GuosiHg <1142570567@qq.com>
2018-04-23 21:01:23 +0800

    //

--- haizeix/一只小蜜蜂.cpp
+++ haizeix/一只小蜜蜂.cpp
@@ -6,6 +6,7 @@ int main()
 {
     int n;
     int i,j;
+    cout << "输入数据组数" << endl;
     scanf("%d",&n);
     a[1] = 0;
     a[2] = 1;
@@ -13,6 +14,7 @@ int main()
     
     while(n--)
     {
+    cout << "输入i，j" << endl;
     scanf("%d%d",&i,&j);
 
     for (int m = 4;m <= j-i+1;m++)
